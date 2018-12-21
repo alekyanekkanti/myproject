d6639270bde3467be9d28237818c8727e35c2d63
alekyanekkanti <46034906+alekyanekkanti@users.noreply.github.com>
2018-12-20 20:10:23 -0600

    Add files via upload

---
+++ readJson.scala
@@ -0,0 +1,42 @@
+
+package com.basic
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.{StructType,StructField,StringType}
+import org.apache.spark.sql._
+import org.apache.commons.io.IOUtils
+import java.net.URL
+import java.nio.charset.Charset
+import org.apache.spark.sql.types.{StructType, StructField, StringType};
+import org.apache.spark.sql.types.{StructType, StructField, LongType};
+
+object readJson {
+
+  case class Example(sno:String,message:String)
+
+  def main(args: Array[String]) ={
+
+    //System.setProperty("hadoop.home.dir", "C:\\hadoop\\");
+    //spark 2
+    val sparkSession = SparkSession.builder
+      .master("local")
+      .appName("my-spark-app")
+      .config("spark.some.config.option", "config-value")
+      .getOrCreate()
+
+    // this is used to implicitly convert an RDD to a DataFrame.
+    import sparkSession.implicits._
+    val erorDF = sparkSession.read.json("C:/alekya/sample.json")
+    erorDF.printSchema()
+    erorDF.registerTempTable("errorTbl")
+    erorDF.show()
+    sparkSession.sql("select sno from errorTbl").show()
+
+
+  }
+}
